f8c4be04165799d4535168c22e06a38987f69b8d
root <luisantoniio1998@gmail.com>
2022-06-16 13:14:13 -0400

    Layouts para listar criados

---
+++ app/src/main/java/com/example/provavinhos/ListarClientes.kt
@@ -0,0 +1,11 @@
+package com.example.provavinhos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListarClientes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_clientes)
+    }
+}
---
+++ app/src/main/java/com/example/provavinhos/VendaUpdateDelete.kt
@@ -0,0 +1,11 @@
+package com.example.provavinhos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VendaUpdateDelete : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venda_update_delete)
+    }
+}
