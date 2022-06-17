dc02cbad32207d281a019ad32a151ccfd6d2efb8
root <luisantoniio1998@gmail.com>
2022-06-17 06:12:38 -0400

    foreign keys corrigidas

--- app/src/main/java/com/example/provavinhos/BD/TabelaBDVendas.kt
+++ app/src/main/java/com/example/provavinhos/BD/TabelaBDVendas.kt
@@ -6,9 +6,9 @@ import android.provider.BaseColumns
 class TabelaBDVendas (db: SQLiteDatabase) : TabelaBD(db, NOME){
     override fun cria() {
         db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "FOREIGN KEY $CAMPO_NOME_VINHO REFERENCES ${TabelaBDVinhos.CAMPO_NOME} ON DELETE RESTRICT," +
-                "FOREIGN KEY $CAMPO_NOME_CLIENTE REFERENCES ${TabelaBDClientes.CAMPO_NOME} ON DELETE RESTRICT," +
-                "FOREIGN KEY $CAMPO_PRECO_GARRAFA REFERENCES ${TabelaBDVinhos.CAMPO_PRECO} ON DELETE RESTRICT,"+
+                "FOREIGN KEY $CAMPO_NOME_VINHO REFERENCES ${TabelaBDVinhos.CAMPO_NOME}(${BaseColumns._ID}) ON DELETE RESTRICT," +
+                "FOREIGN KEY $CAMPO_NOME_CLIENTE REFERENCES ${TabelaBDClientes.CAMPO_NOME}(${BaseColumns._ID}) ON DELETE RESTRICT," +
+                "FOREIGN KEY $CAMPO_PRECO_GARRAFA REFERENCES ${TabelaBDVinhos.CAMPO_PRECO}(${BaseColumns._ID}) ON DELETE RESTRICT,"+
         "$CAMPO_NUMERO INTEGER NOT NULL, $CAMPO_PRECO DOUBLE NOT NULL)")
     }
 
