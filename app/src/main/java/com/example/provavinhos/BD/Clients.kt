6fa35182af27f152cfdd2cc064027dc97f86f618
root <luisantoniio1998@gmail.com>
2022-06-20 18:17:36 -0400

    Criada funcao para converter Cliente num Cursor

--- app/src/main/java/com/example/provavinhos/BD/Clients.kt
+++ app/src/main/java/com/example/provavinhos/BD/Clients.kt
@@ -1,6 +1,8 @@
 package com.example.provavinhos.BD
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Clients(
     var nome: String,
@@ -8,13 +10,29 @@ data class Clients(
     var nif: String,
     var id: Long =-1
 ) {
-    fun toContentValues() : ContentValues{
+    fun toContentValues(): ContentValues {
         val valores = ContentValues()
 
         valores.put(TabelaBDClientes.CAMPO_NOME, nome)
-        valores.put(TabelaBDClientes.CAMPO_CONTACTO, contacto )
+        valores.put(TabelaBDClientes.CAMPO_CONTACTO, contacto)
         valores.put(TabelaBDClientes.CAMPO_NIF, nif)
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Clients {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaBDClientes.CAMPO_NOME)
+            val posContacto = cursor.getColumnIndex(TabelaBDClientes.CAMPO_CONTACTO)
+            val posNif = cursor.getColumnIndex(TabelaBDClientes.CAMPO_NIF)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val contacto = cursor.getString(posContacto)
+            val nif = cursor.getString(posNif)
+
+            return Clients(nome, contacto, nif, id)
+        }
     }
 }
