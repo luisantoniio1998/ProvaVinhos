8fbbdd651d75876ded16f459d70fdb5570728c55
root <luisantoniio1998@gmail.com>
2022-06-21 17:57:46 -0400

    Implementada função query getType e insert no ContentProviderRegioes

--- app/src/main/java/com/example/provavinhos/BD/ContentProviderRegioes.kt
+++ app/src/main/java/com/example/provavinhos/BD/ContentProviderRegioes.kt
@@ -5,12 +5,12 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
 class ContentProviderRegioes : ContentProvider() {
-
-    var db : BDVinhosOpenHelper? = null
+    var dbOpenHelper : BDVinhosOpenHelper? = null
     override fun onCreate(): Boolean {
-        db = BDVinhosOpenHelper(context)
+        dbOpenHelper = BDVinhosOpenHelper(context)
 
         return true
     }
@@ -22,15 +22,49 @@ class ContentProviderRegioes : ContentProvider() {
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        val db = dbOpenHelper!!.readableDatabase
+
+        requireNotNull(projection)
+        val colunas = projection as Array<String>
+
+        val argsSeleccao = selectionArgs as Array<String>?
+
+        val id = uri.lastPathSegment
+
+        val cursor = when (getUriMatcher().match(uri)) {
+            URI_REGIOES -> TabelaBDRegiao(db).query(colunas, selection, argsSeleccao, null, null, sortOrder)
+            URI_REGIAO_ESPECIFICA -> TabelaBDRegiao(db).query(colunas, "${BaseColumns._ID}=?", arrayOf("${id}"), null, null, null)
+            else -> null
+        }
+
+        db.close()
+
+        return cursor
     }
 
-    override fun getType(uri: Uri): String? {
-        TODO("Not yet implemented")
-    }
+    override fun getType(uri: Uri): String? =
+        when (getUriMatcher().match(uri)) {
+            URI_REGIOES -> "$MULTIPLOS_REGISTOS/${TabelaBDRegiao.NOME}"
+            URI_REGIAO_ESPECIFICA -> "$UNICO_REGISTO/${TabelaBDRegiao.NOME}"
+            else -> null
+        }
+
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Not yet implemented")
+        val db = dbOpenHelper!!.writableDatabase
+
+        requireNotNull(values)
+
+        val id = when (getUriMatcher().match(uri)) {
+            URI_REGIOES -> TabelaBDRegiao(db).insert(values)
+            else -> -1
+        }
+
+        db.close()
+
+        if (id == -1L) return null
+
+        return Uri.withAppendedPath(uri, "$id")
     }
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
@@ -51,6 +85,9 @@ class ContentProviderRegioes : ContentProvider() {
 
         const val URI_REGIOES = 100
         const val URI_REGIAO_ESPECIFICA = 101
+
+        const val UNICO_REGISTO = "vnd.android.cursor.item"
+        const val MULTIPLOS_REGISTOS = "vnd.android.cursor.dir"
 
         fun getUriMatcher(): UriMatcher{
             var uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
