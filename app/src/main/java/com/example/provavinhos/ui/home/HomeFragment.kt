693be6700d608e112e4163546c9522d2d8551f4e
root <luisantoniio1998@gmail.com>
2022-06-21 18:56:45 -0400

    Quando clicar no botao list them aula no fragment dos clientes ir para o layout de listar clientes

--- app/src/main/java/com/example/provavinhos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/provavinhos/ui/home/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.provavinhos.R
 import com.example.provavinhos.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -33,6 +35,14 @@ class HomeFragment : Fragment() {
             //textView.text = it
         //}
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonListarClientes.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentListaClientes_to_navigation_home)
+        }
     }
 
     override fun onDestroyView() {
