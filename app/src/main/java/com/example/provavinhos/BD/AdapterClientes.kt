00efe60b12d32dbca72da4675ea254890043839c
root <luisantoniio1998@gmail.com>
2022-06-22 19:19:46 -0400

    Implementada a função getItemCount do AdapterClientes

--- app/src/main/java/com/example/provavinhos/BD/AdapterClientes.kt
+++ app/src/main/java/com/example/provavinhos/BD/AdapterClientes.kt
@@ -78,6 +78,8 @@ class AdapterClientes: RecyclerView.Adapter<AdapterClientes.ViewHolderLivro>() {
      * @return The total number of items in this adapter.
      */
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        if(cursor == null) return 0
+
+        return cursor!!.count
     }
 }
