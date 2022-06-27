d46201f4e8c2f3081b27b0510b94077d2677edb0
root <luisantoniio1998@gmail.com>
2022-06-27 18:22:47 -0400

    Alterado layout para uma navegacao mais simples E para uma adaptacao ao projeto mais simples tambem

--- app/src/main/java/com/example/provavinhos/MainActivity.kt
+++ app/src/main/java/com/example/provavinhos/MainActivity.kt
@@ -1,54 +1,37 @@
 package com.example.provavinhos
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.provavinhos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val buttonEntrar = findViewById<Button>(R.id.buttonEntrar)
-        buttonEntrar.setOnClickListener{entrar()}
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
     }
 
-    private fun entrar() {
-
-        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
-
-        val username = editTextUsername.text.toString()
-        if (username.isBlank()) {
-            editTextUsername.error = getString(R.string.field_mandatory)
-            editTextUsername.requestFocus()
-            return
-        }
-
-        if (username != "admin") {
-            editTextUsername.error = getString(R.string.usr_incorrect)
-            editTextUsername.requestFocus()
-            return
-        }
-
-        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
-
-        val password = editTextPassword.text.toString()
-
-        if (password.isBlank()) {
-            editTextPassword.error = getString(R.string.field_mandatory)
-            editTextPassword.requestFocus()
-            return
-        }
-        if (password != "admin") {
-            editTextPassword.error = getString(R.string.pw_incorrect)
-            editTextPassword.requestFocus()
-            return
-        }
-
-
-        val intent = Intent(this, MainActivity2::class.java)
-        startActivity(intent)
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
