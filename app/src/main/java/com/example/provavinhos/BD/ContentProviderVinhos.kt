2d1bc906498877d5c4be28cceaf04012013bda0a
root <luisantoniio1998@gmail.com>
2022-06-29 19:38:22 -0400

    ContentProviderVinhos.kt com urimatcher e oncreate

--- app/src/main/java/com/example/provavinhos/BD/ContentProviderVinhos.kt
+++ app/src/main/java/com/example/provavinhos/BD/ContentProviderVinhos.kt
@@ -5,11 +5,10 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
-import android.provider.BaseColumns
+import android.database.sqlite.SQLiteOpenHelper
 
-class ContentProviderVinhos : ContentProvider() {
-    var dbOpenHelper : BDVinhosOpenHelper? = null
-
+class ContentProviderVinhos : ContentProvider(){
+    var db : BDVinhosOpenHelper? = null
     /**
      * Implement this to initialize your content provider on startup.
      * This method is called for all registered content providers on the
@@ -38,78 +37,10 @@ class ContentProviderVinhos : ContentProvider() {
      * @return true if the provider was successfully loaded, false otherwise
      */
     override fun onCreate(): Boolean {
-        dbOpenHelper = BDVinhosOpenHelper(context)
-
+        db = BDVinhosOpenHelper(context)
         return true
     }
 
-    /**
-     * Implement this to handle query requests from clients.
-     *
-     *
-     * Apps targeting [android.os.Build.VERSION_CODES.O] or higher should override
-     * [.query] and provide a stub
-     * implementation of this method.
-     *
-     *
-     * This method can be called from multiple threads, as described in
-     * [Processes
- * and Threads]({@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads).
-     *
-     *
-     * Example client call:
-     *
-     *
-     * <pre>// Request a specific record.
-     * Cursor managedCursor = managedQuery(
-     * ContentUris.withAppendedId(Contacts.People.CONTENT_URI, 2),
-     * projection,    // Which columns to return.
-     * null,          // WHERE clause.
-     * null,          // WHERE clause value substitution
-     * People.NAME + " ASC");   // Sort order.</pre>
-     * Example implementation:
-     *
-     *
-     * <pre>// SQLiteQueryBuilder is a helper class that creates the
-     * // proper SQL syntax for us.
-     * SQLiteQueryBuilder qBuilder = new SQLiteQueryBuilder();
-     *
-     * // Set the table we're querying.
-     * qBuilder.setTables(DATABASE_TABLE_NAME);
-     *
-     * // If the query ends in a specific record number, we're
-     * // being asked for a specific record, so set the
-     * // WHERE clause in our query.
-     * if((URI_MATCHER.match(uri)) == SPECIFIC_MESSAGE){
-     * qBuilder.appendWhere("_id=" + uri.getPathLeafId());
-     * }
-     *
-     * // Make the query.
-     * Cursor c = qBuilder.query(mDb,
-     * projection,
-     * selection,
-     * selectionArgs,
-     * groupBy,
-     * having,
-     * sortOrder);
-     * c.setNotificationUri(getContext().getContentResolver(), uri);
-     * return c;</pre>
-     *
-     * @param uri The URI to query. This will be the full URI sent by the client;
-     * if the client is requesting a specific record, the URI will end in a record number
-     * that the implementation should parse and add to a WHERE or HAVING clause, specifying
-     * that _id value.
-     * @param projection The list of columns to put into the cursor. If
-     * `null` all columns are included.
-     * @param selection A selection criteria to apply when filtering rows.
-     * If `null` then all rows are included.
-     * @param selectionArgs You may include ?s in selection, which will be replaced by
-     * the values from selectionArgs, in order that they appear in the selection.
-     * The values will be bound as Strings.
-     * @param sortOrder How the rows in the cursor should be sorted.
-     * If `null` then the provider is free to define the sort order.
-     * @return a Cursor or `null`.
-     */
     override fun query(
         uri: Uri,
         projection: Array<out String>?,
@@ -117,177 +48,40 @@ class ContentProviderVinhos : ContentProvider() {
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-        val db = dbOpenHelper!!.readableDatabase
-
-        requireNotNull(projection)
-        val colunas = projection as Array<String>
-
-        val argsSeleccao = selectionArgs as Array<String>?
-
-        val id = uri.lastPathSegment
-
-        val cursor = when (getUriMatcher().match(uri)) {
-            URI_REGIOES -> TabelaBDVinhos(db).query(colunas, selection, argsSeleccao, null, null, sortOrder)
-            URI_REGIOES -> TabelaBDRegiao(db).query(colunas, selection, argsSeleccao, null, null, sortOrder)
-            URI_VINHO_ESPECIFICO -> TabelaBDVinhos(db).query(colunas, "${BaseColumns._ID}=?", arrayOf("${id}"), null, null, null)
-            URI_REGIAO_ESPECIFICA -> TabelaBDVinhos(db).query(colunas, "${BaseColumns._ID}=?", arrayOf("${id}"), null, null, null)
-            else -> null
-        }
-
-        return cursor
+        TODO("Not yet implemented")
     }
 
-    /**
-     * Implement this to handle requests for the MIME type of the data at the
-     * given URI.  The returned MIME type should start with
-     * `vnd.android.cursor.item` for a single record,
-     * or `vnd.android.cursor.dir/` for multiple items.
-     * This method can be called from multiple threads, as described in
-     * [Processes
- * and Threads]({@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads).
-     *
-     *
-     * Note that there are no permissions needed for an application to
-     * access this information; if your content provider requires read and/or
-     * write permissions, or is not exported, all applications can still call
-     * this method regardless of their access permissions.  This allows them
-     * to retrieve the MIME type for a URI when dispatching intents.
-     *
-     * @param uri the URI to query.
-     * @return a MIME type string, or `null` if there is no type.
-     */
-    override fun getType(uri: Uri): String? =
-        when (getUriMatcher().match(uri)) {
-            URI_REGIOES -> "$MULTIPLOS_REGISTOS/${TabelaBDVinhos.NOME}"
-            URI_REGIOES -> "$MULTIPLOS_REGISTOS/${TabelaBDRegiao.NOME}"
-            URI_VINHO_ESPECIFICO-> "$UNICO_REGISTO/${TabelaBDVinhos.NOME}"
-            URI_REGIAO_ESPECIFICA -> "$UNICO_REGISTO/${TabelaBDRegiao.NOME}"
-            else -> null
-        }
+    override fun getType(uri: Uri): String? {
+        TODO("Not yet implemented")
+    }
 
-    /**
-     * Implement this to handle requests to insert a new row. As a courtesy,
-     * call
-     * [ notifyChange()][ContentResolver.notifyChange] after inserting. This method can be called from multiple
-     * threads, as described in [Processes
- * and Threads](
-      {@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads).
-     *
-     * @param uri The content:// URI of the insertion request.
-     * @param values A set of column_name/value pairs to add to the database.
-     * @return The URI for the newly inserted item.
-     */
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        val db = dbOpenHelper!!.writableDatabase
-
-        requireNotNull(values)
-
-        val id = when (getUriMatcher().match(uri)) {
-            URI_VINHOS -> TabelaBDVinhos(db).insert(values)
-            URI_REGIOES -> TabelaBDRegiao(db).insert(values)
-            else -> -1
-        }
-
-        db.close()
-
-        if (id == -1L) return null
-
-        return Uri.withAppendedPath(uri, "$id")
+        TODO("Not yet implemented")
     }
 
-    /**
-     * Implement this to handle requests to delete one or more rows. The
-     * implementation should apply the selection clause when performing
-     * deletion, allowing the operation to affect multiple rows in a directory.
-     * As a courtesy, call
-     * [ notifyChange()][ContentResolver.notifyChange] after deleting. This method can be called from multiple
-     * threads, as described in [Processes
- * and Threads](
-      {@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads).
-     *
-     *
-     * The implementation is responsible for parsing out a row ID at the end of
-     * the URI, if a specific row is being deleted. That is, the client would
-     * pass in `content://contacts/people/22` and the implementation
-     * is responsible for parsing the record number (22) when creating a SQL
-     * statement.
-     *
-     * @param uri The full URI to query, including a row ID (if a specific
-     * record is requested).
-     * @param selection An optional restriction to apply to rows when deleting.
-     * @return The number of rows affected.
-     * @throws SQLException
-     */
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        val db = dbOpenHelper!!.writableDatabase
-
-        val id = uri.lastPathSegment
-
-        val registosApagados = when (getUriMatcher().match(uri)) {
-            URI_VINHO_ESPECIFICO -> TabelaBDVinhos(db).delete("${BaseColumns._ID}=?", arrayOf("${id}"))
-            URI_REGIAO_ESPECIFICA -> TabelaBDRegiao(db).delete("${BaseColumns._ID}=?", arrayOf("${id}"))
-            else -> 0
-        }
-
-        db.close()
-
-        return registosApagados
+        TODO("Not yet implemented")
     }
 
-    /**
-     * Implement this to handle requests to update one or more rows. The
-     * implementation should update all rows matching the selection to set the
-     * columns according to the provided values map. As a courtesy, call
-     * [ notifyChange()][ContentResolver.notifyChange] after updating. This method can be called from multiple
-     * threads, as described in [Processes
- * and Threads](
-      {@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads).
-     *
-     * @param uri The URI to query. This can potentially have a record ID if
-     * this is an update request for a specific record.
-     * @param values A set of column_name/value pairs to update in the database.
-     * @param selection An optional filter to match rows to update.
-     * @return the number of rows affected.
-     */
     override fun update(
         uri: Uri,
         values: ContentValues?,
         selection: String?,
         selectionArgs: Array<out String>?
     ): Int {
-        requireNotNull(values)
-
-        val db = dbOpenHelper!!.writableDatabase
-
-        val id = uri.lastPathSegment
-
-        val registosAlterados = when (getUriMatcher().match(uri)) {
-            URI_VINHO_ESPECIFICO -> TabelaBDVinhos(db).update(values, "${BaseColumns._ID}=?", arrayOf("${id}"))
-            URI_REGIAO_ESPECIFICA -> TabelaBDRegiao(db).update(values,"${BaseColumns._ID}=?", arrayOf("${id}"))
-            else -> 0
-        }
-
-        db.close()
-
-        return registosAlterados
+        TODO("Not yet implemented")
     }
 
-    companion object {
-        private const val AUTHORITY = "com.example.provavinhos"
+    companion object{
+        const val AUTHORITY = "com.example.provavinhos"
 
-        private const val URI_REGIOES = 100
-        private const val URI_REGIAO_ESPECIFICA = 101
-        private const val URI_VINHOS = 200
-        private const val URI_VINHO_ESPECIFICO = 201
+        const val URI_REGIOES = 100
+        const val URI_REGIAO_ESPECIFICA = 101
+        const val URI_VINHOS = 200
+        const val URI_VINHO_ESPECIFICO = 201
 
-        private const val UNICO_REGISTO = "vnd.android.cursor.item"
-        private const val MULTIPLOS_REGISTOS = "vnd.android.cursor.dir"
+        fun getUriMatcher() : UriMatcher{
 
-        private val ENDERECO_BASE = Uri.parse("content://$AUTHORITY")
-        val ENDERECO_VINHOS = Uri.withAppendedPath(ENDERECO_BASE, TabelaBDVinhos.NOME)
-        //val ENDERECO_CATEGORIAS = Uri.withAppendedPath(ENDERECO_BASE, TabelaBDRegiao.NOME)
-
-        fun getUriMatcher() : UriMatcher {
             var uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
 
             uriMatcher.addURI(AUTHORITY, TabelaBDRegiao.NOME, URI_REGIOES)
--- app/src/main/java/com/example/provavinhos/ListaVendasFragment.kt
+++ app/src/main/java/com/example/provavinhos/ListaVendasFragment.kt
@@ -4,171 +4,27 @@ import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.provavinhos.BD.AdapterVendas
-import com.example.provavinhos.BD.ContentProviderVendas
 import com.example.provavinhos.BD.Sales
 import com.example.provavinhos.BD.TabelaBDVendas
 import com.example.provavinhos.databinding.FragmentListaVendasBinding
 
 class ListaVendasFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
-
-    var vendaSeleccionada: Sales? = null
-        get() = field
-        set(value) {
-            field = value
-            (requireActivity() as MainActivity).mostraOpcoesAlterarEliminar(field != null)
-        }
-
-
-    private var _binding: FragmentListaVendasBinding? = null
-    private var adapterVendas: AdapterVendas? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentListaVendasBinding.inflate(inflater, container, false)
-        return binding.root
-
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        TODO("Not yet implemented")
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        LoaderManager.getInstance(this).initLoader(ID_LOADER_VENDAS, null, this)
-
-        adapterVendas = AdapterVendas(this)
-        binding.recyclerViewVendas.adapter = adapterVendas
-        binding.recyclerViewVendas.layoutManager = LinearLayoutManager(requireContext())
-
-        val activity = activity as MainActivity
-        activity.fragment = this
-        activity.idMenuAtual = R.menu.menu_lista
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    /**
-     * Instantiate and return a new Loader for the given ID.
-     *
-     *
-     * This will always be called from the process's main thread.
-     *
-     * @param id The ID whose loader is to be created.
-     * @param args Any arguments supplied by the caller.
-     * @return Return a new Loader instance that is ready to start loading.
-     */
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
-        CursorLoader(
-            requireContext(),
-            ContentProviderVendas.ENDERECO_VENDAS,
-            TabelaBDVendas.TODAS_COLUNAS,
-            null,
-            null,
-            "${TabelaBDVendas.CAMPO_NOME_CLIENTE}"
-        )
-
-    /**
-     * Called when a previously created loader has finished its load.  Note
-     * that normally an application is *not* allowed to commit fragment
-     * transactions while in this call, since it can happen after an
-     * activity's state is saved.  See [ FragmentManager.openTransaction()][androidx.fragment.app.FragmentManager.beginTransaction] for further discussion on this.
-     *
-     *
-     * This function is guaranteed to be called prior to the release of
-     * the last data that was supplied for this Loader.  At this point
-     * you should remove all use of the old data (since it will be released
-     * soon), but should not do your own release of the data since its Loader
-     * owns it and will take care of that.  The Loader will take care of
-     * management of its data so you don't have to.  In particular:
-     *
-     *
-     *  *
-     *
-     *The Loader will monitor for changes to the data, and report
-     * them to you through new calls here.  You should not monitor the
-     * data yourself.  For example, if the data is a [android.database.Cursor]
-     * and you place it in a [android.widget.CursorAdapter], use
-     * the [android.widget.CursorAdapter.CursorAdapter] constructor *without* passing
-     * in either [android.widget.CursorAdapter.FLAG_AUTO_REQUERY]
-     * or [android.widget.CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER]
-     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
-     * from doing its own observing of the Cursor, which is not needed since
-     * when a change happens you will get a new Cursor throw another call
-     * here.
-     *  *  The Loader will release the data once it knows the application
-     * is no longer using it.  For example, if the data is
-     * a [android.database.Cursor] from a [android.content.CursorLoader],
-     * you should not call close() on it yourself.  If the Cursor is being placed in a
-     * [android.widget.CursorAdapter], you should use the
-     * [android.widget.CursorAdapter.swapCursor]
-     * method so that the old Cursor is not closed.
-     *
-     *
-     *
-     * This will always be called from the process's main thread.
-     *
-     * @param loader The Loader that has finished.
-     * @param data The data generated by the Loader.
-     */
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        adapterVendas!!.cursor = data
+        TODO("Not yet implemented")
     }
 
-    /**
-     * Called when a previously created loader is being reset, and thus
-     * making its data unavailable.  The application should at this point
-     * remove any references it has to the Loader's data.
-     *
-     *
-     * This will always be called from the process's main thread.
-     *
-     * @param loader The Loader that is being reset.
-     */
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        if (_binding == null) return
-        adapterVendas!!.cursor = null
+        TODO("Not yet implemented")
     }
 
-    //fun processaOpcaoMenu(item: MenuItem): Boolean =
-    /* when(item.itemId) {
-            R.id.action_inserir -> {
-                val acao = ListaVendasFragmentDirections.actionListaVendasFragmentToEditarVendaFragment()
-                findNavController().navigate(acao)
-                (activity as MainActivity).atualizaTitulo(R.string.inserir_sale_label)
-                true
-            }
-            R.id.action_alterar -> {
-                val acao = ListaVendasFragmentDirections.actionListaVendasFragmentToEditarVendaFragment(vendaSeleccionada)
-                findNavController().navigate(acao)
-                (activity as MainActivity).atualizaTitulo(R.string.alterar_livro_label)
-                true
-            }
-            R.id.action_eliminar -> {
-                val acao = ListaLivrosFragmentDirections.actionListaLivrosFragmentToEliminarLivroFragment(livroSeleccionado!!)
-                findNavController().navigate(acao)
-                true
-            }
-            else -> false
-        }*/
-
-    companion object {
-        const val ID_LOADER_VENDAS = 0
-    }
 }
